74dd4958c0dcdbd5b1f6d3621b8ce01cd18dba01
Casper Uiterwijk <casper.uiterwijk@gmail.com>
2021-01-02 11:46:02 +0100

    Refactor: Make Percept, Command and Gene enums

--- src/world/agent/genes.rs
+++ src/world/agent/genes.rs
@@ -1,18 +1,21 @@
 use crate::util::{Vec3f, WRng};
 use rand::Rng;
+use strum::EnumCount;
+use strum_macros::{AsRefStr, EnumCount as EnumCountMacro, EnumIter};
 use vek::ops::Lerp;
 
-const N_GENES: usize = 9;
-
-const GENE_MUTATION_FACTOR: usize = 0;
-const GENE_COLOR_R: usize = 1;
-const GENE_COLOR_G: usize = 2;
-const GENE_COLOR_B: usize = 3;
-const GENE_SIZE: usize = 4;
-const GENE_SPEED: usize = 5;
-const GENE_EYE_DISTANCE: usize = 6;
-const GENE_EYE_ANGLE: usize = 7;
-const GENE_TIMER_INTERVAL: usize = 8;
+#[derive(Copy, Clone, PartialEq, EnumCountMacro, EnumIter, AsRefStr)]
+pub enum Gene {
+    MutationFactor = 0,
+    ColorR = 1,
+    ColorG = 2,
+    ColorB = 3,
+    Size = 4,
+    Speed = 5,
+    EyeDistance = 6,
+    EyeAngle = 7,
+    TimerInterval = 8,
+}
 
 const MAX_MUTATION_FACTOR: f32 = 0.1;
 const MIN_SIZE: f32 = 2.5;
@@ -29,15 +32,15 @@ const MAX_TIMER_INTERVAL: f32 = 50.0;
 /// Struct that keeps track of an agent's genes.
 /// Has methods for determining agent attributes.
 pub struct Genes {
-    genes: [f32; N_GENES],
+    genes: [f32; Gene::COUNT],
 }
 
 impl Genes {
     /// Generates random genes.
     pub fn new_random(rng: &mut WRng) -> Genes {
-        let mut genes = [0.0; N_GENES];
+        let mut genes = [0.0; Gene::COUNT];
 
-        for i in 0..N_GENES {
+        for i in 0..Gene::COUNT {
             genes[i] = rng.gen::<f32>();
         }
 
@@ -47,7 +50,7 @@ impl Genes {
     /// Reproduces the genes asexually, mutating according to the mutation factor.
     pub fn reproduce(&self, rng: &mut WRng) -> Genes {
         let mut new_genes = self.genes.clone();
-        for i in 0..N_GENES {
+        for i in 0..Gene::COUNT {
             new_genes[i] += (rng.gen::<f32>() * 2.0 - 1.0) * self.get_mutation_factor();
         }
 
@@ -55,38 +58,42 @@ impl Genes {
     }
 
     pub fn get_mutation_factor(&self) -> f32 {
-        self.genes[GENE_MUTATION_FACTOR] * MAX_MUTATION_FACTOR
+        self.genes[Gene::MutationFactor as usize] * MAX_MUTATION_FACTOR
     }
 
     pub fn get_color(&self) -> Vec3f {
         Vec3f::new(
-            self.genes[GENE_COLOR_R],
-            self.genes[GENE_COLOR_G],
-            self.genes[GENE_COLOR_B],
+            self.genes[Gene::ColorR as usize],
+            self.genes[Gene::ColorG as usize],
+            self.genes[Gene::ColorB as usize],
         )
     }
 
     pub fn get_size(&self) -> f32 {
-        Lerp::lerp(MIN_SIZE, MAX_SIZE, self.genes[GENE_SIZE])
+        Lerp::lerp(MIN_SIZE, MAX_SIZE, self.genes[Gene::Size as usize])
     }
     pub fn get_speed(&self) -> f32 {
-        Lerp::lerp(MIN_SPEED, MAX_SPEED, self.genes[GENE_SPEED])
+        Lerp::lerp(MIN_SPEED, MAX_SPEED, self.genes[Gene::Speed as usize])
     }
     pub fn get_eye_distance(&self) -> f32 {
         Lerp::lerp(
             MIN_EYE_DISTANCE,
             MAX_EYE_DISTANCE,
-            self.genes[GENE_EYE_DISTANCE],
+            self.genes[Gene::EyeDistance as usize],
         )
     }
     pub fn get_eye_angle(&self) -> f32 {
-        Lerp::lerp(MIN_EYE_ANGLE, MAX_EYE_ANGLE, self.genes[GENE_EYE_ANGLE])
+        Lerp::lerp(
+            MIN_EYE_ANGLE,
+            MAX_EYE_ANGLE,
+            self.genes[Gene::EyeAngle as usize],
+        )
     }
     pub fn get_timer_interval(&self) -> f32 {
         Lerp::lerp(
             MIN_TIMER_INTERVAL,
             MAX_TIMER_INTERVAL,
-            self.genes[GENE_TIMER_INTERVAL],
+            self.genes[Gene::TimerInterval as usize],
         )
     }
 }
